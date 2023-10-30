2c1f6609316a74d795a01fde7343910f17d12d0d
Иван Сидоров <ivsidorov@www.mts.ru>
2023-10-31 00:45:35 +0300

    HW1. Attempt 2.

--- 01/Allocator.cpp
+++ 01/Allocator.cpp
@@ -1,6 +1,8 @@
 #include "Allocator.hpp"
 
 void Allocator::MakeAllocator(size_t max_size) {
+    // если идет попытка заново создать аллокатор,
+    // необходимо почистить выделенную до этого память
     delete[] this->data_;
 
     this->data_ = new char[max_size];
@@ -9,13 +11,25 @@ void Allocator::MakeAllocator(size_t max_size) {
 }
 
 char* Allocator::Alloc(size_t size) {
+    // если текущий сдвиг + размер выделяемы памяти превышает допустимый
+    // или попытка выделить память размером 0, возвращаем nullptr
     if ((this->offset_ + size > this->max_size_) || (size == 0)) {
         return nullptr;
     }
+
+    char* result = nullptr;
+    // выделяем память с учетом сдвига (уже выделенной)
+    result = this->data_ + this->offset_;
+    // обновляем размер сдвига
     this->offset_ += size;
-    return this->data_ + size;
+
+    return result;
 }
 
-void Allocator::Reset() { this->offset_ = 0; }
+void Allocator::Reset() {
+    // при сбросе выделенной памяти просто обнуляем сдвиг
+    // память оставляем выделенной для дальнейшего использования
+    this->offset_ = 0;
+}
 
 Allocator::~Allocator() { delete[] this->data_; }
--- 01/tests.cpp
+++ 01/tests.cpp
@@ -10,8 +10,15 @@ TEST(AllocatorTest, MakeAllocatorTest) {
 TEST(AllocatorTest, AdditionalAllocTest) {
     Allocator allocator;
     allocator.MakeAllocator(5);
-    EXPECT_NE(nullptr, allocator.Alloc(3));
-    EXPECT_NE(nullptr, allocator.Alloc(2));
+    char* first = allocator.Alloc(2);
+    char* second = allocator.Alloc(2);
+
+    // ожидается, что оба указателя не null, так как переполнения не было
+    EXPECT_NE(nullptr, first);
+    EXPECT_NE(nullptr, second);
+
+    // проверка на то, что выделяется не один и тот же участок памяти
+    EXPECT_NE(first, second);
 }
 
 TEST(AllocatorTest, AllocOverflowTest) {
@@ -19,6 +26,8 @@ TEST(AllocatorTest, AllocOverflowTest) {
     allocator.MakeAllocator(5);
     EXPECT_EQ(nullptr, allocator.Alloc(6));
 
+    // при первом аллоцировании все должно быть ок
+    // при втором должно вознкнуть переполнение и вернуться nullptr
     EXPECT_NE(nullptr, allocator.Alloc(3));
     EXPECT_EQ(nullptr, allocator.Alloc(3));
 }
@@ -27,6 +36,8 @@ TEST(AllocatorTest, ResetTest) {
     Allocator allocator;
     allocator.MakeAllocator(5);
     EXPECT_NE(nullptr, allocator.Alloc(3));
+    // если выделенная память очистилась, то получится выделить
+    // еще раз память размером 3, и не вернутся nullptr
     allocator.Reset();
     EXPECT_NE(nullptr, allocator.Alloc(3));
 }
@@ -34,6 +45,8 @@ TEST(AllocatorTest, ResetTest) {
 TEST(AllocatorTest, ZeroAllocTest) {
     Allocator allocator;
     allocator.MakeAllocator(5);
+    // при попытке выделить память размером 0
+    // ожидается nullptr
     EXPECT_EQ(nullptr, allocator.Alloc(0));
 }
 
